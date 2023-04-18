c7d348843f66b410248ce7613732c7ec70a5cb44
matthewchunqed <108086692+matthewchunqed@users.noreply.github.com>
2023-04-18 15:22:37 -0400

    Add files via upload

--- tiles.js
+++ tiles.js
@@ -1,11 +1,18 @@
 /*jslint browser:true */
 /*jshint esversion: 6 */
-var boxes = []
+var boxes = [];
 var head = null;
 var width = 0;
 var height = 0;
-var step = 0;
+var head = 0;
+var tail = -1;
 var path = [];
+var resetPoint = 0;
+var buttons = null;
+var gridWidth = 500;
+var gridHeight = 500;
+var boxWidth = gridWidth/width - 2;
+var boxHeight = gridHeight/height - 2;
 
 function drawTiles(width, height) {
     this.width = width;
@@ -13,17 +20,12 @@ function drawTiles(width, height) {
     var grid = document.querySelector("#grid"), i, div;
     var entire = document.querySelector(".entire")
 
-    var gridWidth = 500;
-    var gridHeight = 500;
-
     grid.style.width = gridWidth + "px";
     grid.style.height = gridHeight + "px";
     entire.style.width = (gridWidth+100) + "px";
     entire.style.height = (gridHeight+100) + "px";
     //formats entire grid + gray background around it.
 
-    var boxWidth = gridWidth/width - 2;
-    var boxHeight = gridHeight/height - 2;
     //size of cells
     
     var s = "auto";
@@ -58,39 +60,85 @@ function drawTiles(width, height) {
         grid.appendChild(div);
     }
 
-}
-
-function start() {
-
-}
-
-function getPath(){
-
-        for(var i=0; i < width; i++){
+    //sets up what the correct path is algorithmically.
+    if(width % 2 == 0){
+        //case 1: even length
+        for(i=0; i < width; i++){
             path.push(boxes[0][i]);
         }
-        for(var i=width-1; i >= 0; i = i-1){
+        for(i=width-1; i >= 0; i = i-1){
             if(i % 2 == 0){
-                for(var j=width-1; j >= 0; j = j-1){
+                for(var j=height-1; j >= 1; j = j-1){
                     path.push(boxes[j][i]);
                 }
             }else{
-                for(var j=0; j < width; j++){
+                for(var j=1; j < height; j++){
                     path.push(boxes[j][i]);
                 }
             }
         }
-
-}
-
-function step() {
-    if(getPath == []){
-        getPath();
+    }else if(height % 2 == 0){
+        //case 2: odd length but even height
+        for(var p=0; p < height; p++){
+            // path.push(boxes[p][0]);
+            path.push(boxes[0][0]);
+            path.push(boxes[0][1]);
+            path.push(boxes[0][2]);
+            path.push(boxes[0][3]);
+        }
+        // for(i=height-1; i >= 0; i = i-1){
+        //     if(i % 2 == 0){
+        //         for(var j=width-1; j >= 1; j = j-1){
+        //             path.push(boxes[i][j]);
+        //             console.log(i + " " + j);
+        //         }
+        //     }else{
+        //         for(var j=1; j < width; j++){
+        //             path.push(boxes[i][j]);
+        //             console.log(i + " " + j);
+        //         }
+        //     }
+        // }
     }
-    path[0].style.backgroundColor = "green";
-    step = step + 1;
-}
+    
 
-function animate() {
+    //create start button
+    var button = document.createElement("button");
+    button.innerHTML = "Start";
+    button.addEventListener("mousedown", event => {
+        //functionality for start button
 
+
+    }, false);
+    entire.appendChild(button);
+    button = document.createElement("button");
+    button.innerHTML = "Step";
+    button.addEventListener("mousedown", event => {
+        //functionality for start button
+
+        path[head].style.backgroundColor = "green";
+        if(tail >= 0){
+            path[tail].style.backgroundColor = "white";
+        }
+        head = head + 1;
+        tail = tail + 1;
+        if(head >= path.length){
+            head = 0;
+        }
+        if(tail >= path.length){
+            tail = 0;
+        }
+
+
+      }, false);
+
+
+    entire.appendChild(button);
+    button = document.createElement("button");
+    button.innerHTML = "Animate";
+    button.addEventListener("mousedown", event => {
+        //functionality for start button
+
+    }, false);
+    entire.appendChild(button);
 }
